723023027df7e9fd3539aa9bf2faa3d8ecb1a3ea
Lawson Lay <lawsonlay04@gmail.com>
2023-11-10 17:40:45 -0600

    Folder Creation

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Code and Cusine</h1>
+<p>Hyperlinks to new pages: </p>
+<a href="/menu">Menu</a>
+<a href="/deals">Deals</a>
+<a href="/reservation">Reservation</a>
+<a href="/cart">Cart</a>
+
+
---
+++ myapp/src/routes/menu/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { PageData } from '../test1/$types';
+    
+    export let data: PageData;
+</script>
+
+
+<h1>Menu</h1>
+<button>Okay</button>
