b4cc20838a3e379c9bf41e32f33677f5f5b5d3bc
Lawson Lay <lawsonlay04@gmail.com>
2023-11-10 18:19:45 -0600

    Co-authored-by: Katy <soddykathryn@users.noreply.github.com> Co-authored-by: FatmaB4 <FatmaB4@users.noreply.github.com>

---
+++ myapp/src/routes/menu/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    
+    export let data: LayoutData;
+</script>
+something
+<slot />
+<nav>
+    <h1>
+        does this work
+
+    </h1>
+</nav>
+
+
+
+
+
+
