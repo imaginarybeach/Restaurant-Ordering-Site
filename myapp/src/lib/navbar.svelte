3073fae9c6bf06044f1625b575192074ea6f359d
soddykathryn <soddykathryn@gmail.com>
2024-01-27 17:26:37 -0600

    h

--- myapp/src/lib/navbar.svelte
+++ myapp/src/lib/navbar.svelte
@@ -1,13 +1,22 @@
 <script>
-    export let data;
-    let windowWidth = window.innerWidth;
-    let clicked = false;
+    export let title = "Nav Bar c:"
 </script>
 
-<h1>
-    nav bar :D
-</h1>
+<div class="title">
+    <img src="/logo.ico" alt="site logo">
+    <h1>{title}</h1>
+
+</div>
 
 <style>
-    
+    .title{
+        display: flex;
+        background-color: rgb(196, 181, 216);
+    }
+    .title img{
+        display: inline-block;
+    }
+    .title h1{
+        margin-left: 20px;
+    }
 </style>
