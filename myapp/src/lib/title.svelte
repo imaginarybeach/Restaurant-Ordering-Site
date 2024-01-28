85676c5be2fc7efd4145500db076fc25f6f0248e
soddykathryn <soddykathryn@gmail.com>
2024-01-27 18:35:56 -0600

    NAV BAR

--- myapp/src/lib/title.svelte
+++ myapp/src/lib/title.svelte
@@ -2,10 +2,16 @@
     export let title = ""
 </script>
 
+<div class="logo">
+    <img src="/favicon.png" alt="site logo"
+        width="50"
+        height="50"/>
+
+</div>
+
 <div class="title">
-    <!-- <img src="/logo.ico" alt="site logo"> -->
     <!-- <h1>{title}</h1> -->
-    <h1>Code and Cusine</h1>
+    <a href="/"><b>Code and Cuisine</b></a>
     <a href="/menu">Menu</a>
     <a href="/deals">Deals</a>
     <a href="/reservation">Reservation</a>
@@ -15,13 +21,26 @@
 
 <style>
     .title{
-        display: flex;
+        display:flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        justify-content: space-around;
         background-color: rgb(196, 181, 216);
     }
     .title img{
-        display: inline-block;
+        display: flex;
+
     }
     .title h1{
         margin-left: 20px;
+    }
+    .title a{
+        font-size: 30px;
+        font-family:Georgia, 'Times New Roman', Times, serif;
+        font-style:oblique;
+
+    }
+    .logo img{
+        background-image: "/favicon.png";
     }
 </style>
