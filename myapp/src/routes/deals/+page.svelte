8a3090d8671153bddf8ebe1081fe877c10db2160
soddykathryn <soddykathryn@gmail.com>
2024-02-10 17:10:07 -0600

    deals

--- myapp/src/routes/deals/+page.svelte
+++ myapp/src/routes/deals/+page.svelte
@@ -1,7 +1,7 @@
 <!-- npm run dev -->
 <!-- reactive javascript code goes in script-->
 <script>
-    import Title from '$lib/title.svelte'
+    import Title from '$lib/title.svelte';
 
 </script>
 
@@ -12,36 +12,5 @@
 <main>
     <h1>Deals</h1>
     
-    <hr>
-
-    <div class="flex-container">
-        
-        <div>
-            <h2>Sandwich and Drink Combo</h2>
-            <img src={Sandwich} alt="Sandwich"/>
-            <button type="button" class="perfect-flex" onclick="location.href='/';">
-                Order
-            </button>
-        </div>
-        
-        <div>
-            <h2>Lunch Special</h2>
-            <img src={Pasta} alt="Pasta"/>
-            <button type="button" class="perfect-flex" onclick="location.href='/';">
-                Order
-            </button>
-        </div>
-
-        <div>
-            <h2>Free Refills</h2>
-            <img src={Coffee} alt="Coffee"/>
-            <button type="button" class="perfect-flex" onclick="location.href='/';">
-                Order
-            </button>
-        </div>
-
-
-        
-    </div>    
     
 </main>
