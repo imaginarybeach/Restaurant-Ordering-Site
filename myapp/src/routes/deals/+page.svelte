7ccf452c2daced5383c7c1fe6da17cca546b3946
soddykathryn <soddykathryn@gmail.com>
2024-03-23 11:36:11 -0500

    improved deals

--- myapp/src/routes/deals/+page.svelte
+++ myapp/src/routes/deals/+page.svelte
@@ -16,9 +16,25 @@
 <main>
     <div class="pasta-image">
         <div class="pasta-text">
-            <h1 style="font-size:50px">Exclusive Deal!</h1>
-            <h2>50% off on Pasta Dishes</h2>
-            <button type="button" class="perfect-flex" onclick="location.href='/';">Add to Order</button>
+            <h1 style="font-size:100px">Exclusive Deal!</h1>
+            <h2 style="font-size:30px">50% off on Pasta Dishes</h2>
+            <button type="button" class="perfect-flex" onclick="location.href='/menu';">Add to Order</button>
+        </div>
+    </div>
+
+    <div class="tea-image">
+        <div class="tea-text">
+            <h1 style="font-size:100px">I'm Thirsty!</h1>
+            <h2 style="font-size:30px">Free Refills on Tea and Soda</h2>
+            <button type="button" class="perfect-flex" onclick="location.href='/menu';">Add to Order</button>
+        </div>
+    </div>
+
+    <div class="soup-image">
+        <div class="soup-text">
+            <h1 style="font-size:100px">Craving More?</h1>
+            <h2 style="font-size:30px">Get a Soup to Go</h2>
+            <button type="button" class="perfect-flex" onclick="location.href='/menu';">Add to Order</button>
         </div>
     </div>
     
@@ -73,6 +89,47 @@
         position: relative;
     }
     .pasta-text {
+        font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+        text-align: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: rgb(145, 117, 158);
+        -webkit-text-stroke-width: 0.5px;
+        -webkit-text-stroke-color: rgb(233, 226, 238);
+        
+    }
+
+    .tea-image {
+        background-image: url(https://i.mscwlns.co/mosaic-wellness/image/upload/f_auto,w_1000,c_limit/v1645021053/BW%20BLOG/Untitled-design---2022-02-16T194724.132_11zon.jpg);
+        height: 500px;
+        background-position:center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        position: relative;
+    }
+    .tea-text {
+        font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+        text-align: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: rgb(145, 117, 158);
+        -webkit-text-stroke-width: 0.5px;
+        -webkit-text-stroke-color: rgb(233, 226, 238);
+        
+    }
+    .soup-image {
+        background-image: url(https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2023/10/Chicken-Soup-main-2.jpg);
+        height: 500px;
+        background-position:center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        position: relative;
+    }
+    .soup-text {
         font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
         text-align: center;
         position: absolute;
